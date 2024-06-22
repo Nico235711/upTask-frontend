1963e79219d0a66abc3e8a80e23b9db8ac915630
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-22 14:28:54 -0300

    Creando el Layout de la aplicación

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,14 +1,26 @@
 import { Outlet } from "react-router-dom"
+import Logo from "../components/Logo"
 
 const AppLayout = () => {
 
   return (
     <>
-      <div>AppLayout</div>
+      <header className="bg-gray-800 p-5">
+        <div className="max-w-6xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+          <div className="w-64">
+            <Logo />
+          </div>
+        </div>
+      </header>
       
-      <main>
+      <section className="max-w-6xl mx-auto mt-20 p-5">
         <Outlet />
-      </main>
+      </section>
+
+
+      <footer className="py-5 bg-gray-800">
+        <p className="text-center text-white text-lg">Todos los derechos reservados {new Date().getFullYear()}</p>
+      </footer>
     </>
   )
 }
