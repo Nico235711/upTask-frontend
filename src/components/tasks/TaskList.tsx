48047158738c3dfbb139d497d250ff8b48b9d482
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-27 18:32:48 -0300

    Aplicando diferentes colores a los estados

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -25,6 +25,14 @@ const statusTanslations: { [keyof: string]: string } = {
   completed: "Completada"
 }
 
+const statusStyles: { [keyof: string]: string } = {
+  pending: "border-t-red-500",
+  onHold: "border-t-yellow-500",
+  inProgress: "border-t-blue-500",
+  underReview: "border-t-purple-500",
+  completed: "border-t-green-500"
+}
+
 const TaskList = ({ tasks }: TaskListProps) => {
 
   const groupedTasks = tasks.reduce((acc, task) => {
@@ -33,18 +41,15 @@ const TaskList = ({ tasks }: TaskListProps) => {
     return { ...acc, [task.status]: currentGroup };
   }, initialStatusGroups);
 
-  // console.log(groupedTasks);
-
-
   return (
     <>
-      <h2 className="text-5xl font-black my-10">Tareas</h2>
+      <h2 className=" border-t text-5xl font-black my-10">Tareas</h2>
 
       <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
             <h3
-              className={`capitalize border-t-8 font-light text-xl border border-slate-300 bg-white p-3`}>{statusTanslations[status]}</h3>
+              className={`capitalize border-t-8 font-light text-xl border border-slate-300 bg-white p-3 ${statusStyles[status]}`}>{statusTanslations[status]}</h3>
             <ul className='mt-5 space-y-5'>
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
